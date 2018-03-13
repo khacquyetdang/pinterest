663a3144aa2b505700d57b8fad261da7dcc7819f
mrdang <khacquyet.dang.pro@gmail.com>
2018-03-13 10:50:15 +0100

    [frontend] add image viewer

--- app/components/Gallery/index.js
+++ app/components/Gallery/index.js
@@ -8,102 +8,150 @@ import React from 'react';
 // import styled from 'styled-components';
 import Masonry from 'react-masonry-component';
 import Gravatar from 'react-gravatar';
-import { FormattedMessage } from 'react-intl';
-import { Button, Modal } from 'react-bootstrap';
+import {FormattedMessage} from 'react-intl';
+import {Button, Modal} from 'react-bootstrap';
 import messages from './messages';
 import messagesApp from './../../containers/App/messages';
-
+import Viewer from 'react-viewer';
+import 'react-viewer/dist/index.css';
 import './styles.scss';
 
 class Gallery extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
 
-    this.state = { deleteId: "" };
+    this.state = {
+      deleteId: "",
+      viewerVisible: false,
+      activeIndex: 0,
+      images: []
+    };
   }
-  handleLayoutComplete = () => {
-
-  }
+  handleLayoutComplete = () => {}
 
   componentDidMount = () => {
-    this.masonry.on('layoutComplete', this.handleLayoutComplete);
+    this
+      .masonry
+      .on('layoutComplete', this.handleLayoutComplete);
   };
+  componentWillReceiveProps(nextProps){
+    var images = nextProps
+      .elements
+      .map((element, index) => {
+        return {src: element.url, alt: element.description, id: element._id}
+      });
 
+    this.setState({
+      images : images
+    });
+  }
   componentWillUnmount = () => {
-    this.masonry.off('layoutComplete', this.handleLayoutComplete);
+    this
+      .masonry
+      .off('layoutComplete', this.handleLayoutComplete);
   }
 
   onVoteClick = (photoId) => {
-    this.props.onVote(photoId);
+    this
+      .props
+      .onVote(photoId);
   }
   onDeleteClick = (photoId) => {
-    this.setState({ deleteId: photoId });
+    this.setState({deleteId: photoId});
   }
   onDeleteConfirm = () => {
-    this.props.onDelete(this.state.deleteId);
-    this.setState({ deleteId: "" });
+    this
+      .props
+      .onDelete(this.state.deleteId);
+    this.setState({deleteId: ""});
   }
 
   handleHide = () => {
-    this.setState({ deleteId: "" });
+    this.setState({deleteId: ""});
   }
+  handleClick = (imgId) => {
+    console.log("mansory item click ", imgId);
+    const activeIndex = this.state.images.findIndex(img => img.id === imgId);
+    this.setState({viewerVisible: true,
+      activeIndex : activeIndex
+    });
+  };
   render() {
     const onVote = this.props.onVote;
     const onDelete = this.props.onDelete;
     const onDeleteClick = this.onDeleteClick;
-    var childElements = this.props.elements.map(function (element, index) {
-      return (
-        <div key={index} className="grid-item">
-          <img className="grid-img" src={element.url} />
-          <div className="description">{element.description}</div>
-          <div className="toolbarphoto">
-            <Gravatar
-              default="https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png" className="img-responsive " email={element.owner.email} />
 
-            {onDelete ?
-              <Button bsSize="small"
-                onClick={() => { onDeleteClick(element._id) }}>
-                <span className="glyphicon glyphicon-trash"> </span>
-              </Button> : null}
+    var childElements = this
+      .props
+      .elements
+      .map((element, index) => {
+        return (
+          <div key={index} className="grid-item">
+            <img
+              className="grid-img"
+              onClick={() => this.handleClick(element._id)}
+              src={element.url}/>
+            <div className="description">{element.description}</div>
+            <div className="toolbarphoto">
+              <Gravatar
+                default="https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png"
+                className="img-responsive "
+                email={element.owner.email}/> {onDelete
+                ? <Button
+                    bsSize="small"
+                    onClick={() => {
+                    onDeleteClick(element._id)
+                  }}>
+                    <span className="glyphicon glyphicon-trash"></span>
+                  </Button>
+                : null}
 
-            <Button bsSize="small"
-              onClick={() => { onVote(element._id) }}>
-              <span className="glyphicon glyphicon-star"> </span>
-              {element.likeCount}
-            </Button>
+              <Button
+                bsSize="small"
+                onClick={() => {
+                onVote(element._id)
+              }}>
+                <span className="glyphicon glyphicon-star"></span>
+                {element.likeCount}
+              </Button>
+            </div>
           </div>
-        </div>
-      );
-    });
+        );
+      });
     return (
       <div>
-        <Masonry className="grid"
-          ref={function (c) { this.masonry = this.masonry || c.masonry; }.bind(this)}
-        >
+        <Masonry
+          className="grid"
+          ref={function (c) {
+          this.masonry = this.masonry || c.masonry;
+        }.bind(this)}>
           {childElements}
         </Masonry>
-        <Modal
-          show={this.state.deleteId !== ""}
-          onHide={this.handleHide}
-        >
+        <Viewer
+          visible={this.state.viewerVisible}
+          activeIndex={this.state.activeIndex}
+          onClose={() => {
+          this.setState({viewerVisible: false});
+        }}
+          images={this.state.images}/>
+        <Modal show={this.state.deleteId !== ""} onHide={this.handleHide}>
           <Modal.Header closeButton>
             <Modal.Title>
-              <FormattedMessage {...messages.deleteTitle} />
+              <FormattedMessage {...messages.deleteTitle}/>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <h1>
-              <FormattedMessage {...messages.deleteConfirm } />
+              <FormattedMessage {...messages.deleteConfirm }/>
             </h1>
           </Modal.Body>
           <Modal.Footer>
             <Button bsStyle="primary" onClick={this.handleHide}>
-              <FormattedMessage {...messagesApp.cancel } />
+              <FormattedMessage {...messagesApp.cancel }/>
             </Button>
             <Button bsStyle="danger" onClick={this.onDeleteConfirm}>
-              <FormattedMessage {...messagesApp.valid } />
+              <FormattedMessage {...messagesApp.valid }/>
             </Button>
-
           </Modal.Footer>
         </Modal>
       </div>
@@ -111,8 +159,6 @@ class Gallery extends React.Component { // eslint-disable-line react/prefer-stat
   }
 }
 
-Gallery.propTypes = {
-
-};
+Gallery.propTypes = {};
 
 export default Gallery;
