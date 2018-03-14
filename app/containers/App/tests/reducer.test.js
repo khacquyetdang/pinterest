a346291cc24264128cfc163da70b4cc981faa4d2
mrdang <khacquyet.dang.pro@gmail.com>
2018-03-14 16:04:57 +0100

    in progress

--- app/containers/App/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import appReducer from '../reducer';
-
-describe('appReducer', () => {
-  it('returns the initial state', () => {
-    expect(appReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- app/containers/App/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectAppDomain } from '../selectors';
-
-describe('selectAppDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
